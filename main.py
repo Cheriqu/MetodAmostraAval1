a5cbf79bb6c983b447d107333b4ef0776071c869
cheri <cheriqueluiz@gmail.com>
2022-12-28 14:25:21 -0300

    Commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ anos = [21,18,20,21,30,25,11,21,18,29,12,22,25,10,29,23,15,16,18,28]
 media = st.mean(anos)
 var = round(st.variance(anos),2)
 
-print('Media:', media, '\t Variancia:', var)
+print('Media:', media, '\n Variancia:', var)
 
 i1 , i2, i3, i4, i5, i6, i7, i8, i9, i10 = range(10,12,1), range(12,14,1), range(14,16,1), range(16,18,1), range(18,20,1), \
                                       range(20,22,1), range(22,24,1), range(24,26,1), range(26,28,1), range(28, max(anos), 1)
@@ -53,7 +53,7 @@ A['%'] = A['f']/20*100
 A.set_axis(['Valores','f','%'], axis='columns', inplace=True)
 A.set_axis(['10 |-- 12','12 |-- 14','14 |-- 16','16 |-- 18','18 |-- 20','20 |-- 22','22 |-- 24',
             '24 |-- 26','26 |-- 28','28 ou mais'], axis='rows', inplace=True)
-print(A)
+print("\n",A)
 
 n=2
 df = pd.DataFrame()
@@ -121,3 +121,122 @@ percent_media_pop = round(df[(df['Int. Inf.'] < media) & (df['Int. Sup.'] > medi
 print("\nn:", n,"\nNúmero de amostras selecionadas:",num_amostras,"\nMédia das médias:",media_medias,"\nMédia das variâncias:",
       medias_variancias,"\nMenor média:",menor_media,"\nMaior média:",maior_media,"\nAmplitude:",amplitude,
       "\nPercentual de intervalos que contém a média populacional:", percent_media_pop)
+print("\n")
+
+d2 = pd.DataFrame()
+n = 2
+d2['Amostras2'] = list(combinations(anos, n))
+l1=l2=l3=l4=l5=l6=l7=l8=l9=l10 = 0
+count = 0
+for i in range(0,len(d2['Amostras2'])):
+    for j in range(0,2):
+        if d2['Amostras2'][i][j] in i1:
+            l1 = l1 + 1
+        elif d2['Amostras2'][i][j] in i2:
+            l2 = l2 + 1
+        elif d2['Amostras2'][i][j] in i3:
+            l3 = l3 + 1
+        elif d2['Amostras2'][i][j] in i4:
+            l4 = l4 + 1
+        elif d2['Amostras2'][i][j] in i5:
+            l5 = l5 + 1
+        elif d2['Amostras2'][i][j] in i6:
+            l6 = l6+ 1
+        elif d2['Amostras2'][i][j] in i7:
+            l7 = l7 + 1
+        elif d2['Amostras2'][i][j] in i8:
+            l8 = l8 + 1
+        elif d2['Amostras2'][i][j] in i9:
+            l9 = l9 + 1
+        elif d2['Amostras2'][i][j] in i10:
+            l10 = l10 + 1
+
+B = pd.DataFrame()
+B = pd.DataFrame(list([[l1],[l2],[l3],[l4],[l5],[l6],[l7],[l8],[l9],[l10]]))
+B.set_axis(['10 |-- 12','12 |-- 14','14 |-- 16','16 |-- 18','18 |-- 20','20 |-- 22','22 |-- 24',
+            '24 |-- 26','26 |-- 28','28 ou mais'], axis='rows', inplace=True)
+B.set_axis(['Valores'], axis='columns', inplace=True)
+B['Valores'] = A['Valores']
+B['Freq. Abs. (n=2)'] = l1,l2,l3,l4,l5,l6,l7,l8,l9,l10
+B['Freq. % (n=2)'] = round(B['Freq. Abs. (n=2)'] / sum(B['Freq. Abs. (n=2)'])*100,2)
+print(B)
+
+
+d3 = pd.DataFrame()
+n = 3
+d3['Amostras3'] = list(combinations(anos, n))
+l1=l2=l3=l4=l5=l6=l7=l8=l9=l10 = 0
+count = 0
+for i in range(0,len(d3['Amostras3'])):
+    for j in range(0,2):
+        if d3['Amostras3'][i][j] in i1:
+            l1 = l1 + 1
+        elif d3['Amostras3'][i][j] in i2:
+            l2 = l2 + 1
+        elif d3['Amostras3'][i][j] in i3:
+            l3 = l3 + 1
+        elif d3['Amostras3'][i][j] in i4:
+            l4 = l4 + 1
+        elif d3['Amostras3'][i][j] in i5:
+            l5 = l5 + 1
+        elif d3['Amostras3'][i][j] in i6:
+            l6 = l6+ 1
+        elif d3['Amostras3'][i][j] in i7:
+            l7 = l7 + 1
+        elif d3['Amostras3'][i][j] in i8:
+            l8 = l8 + 1
+        elif d3['Amostras3'][i][j] in i9:
+            l9 = l9 + 1
+        elif d3['Amostras3'][i][j] in i10:
+            l10 = l10 + 1
+
+print("\n")
+C = pd.DataFrame()
+C = pd.DataFrame(list([[l1],[l2],[l3],[l4],[l5],[l6],[l7],[l8],[l9],[l10]]))
+C.set_axis(['10 |-- 12','12 |-- 14','14 |-- 16','16 |-- 18','18 |-- 20','20 |-- 22','22 |-- 24',
+            '24 |-- 26','26 |-- 28','28 ou mais'], axis='rows', inplace=True)
+C.set_axis(['Valores'], axis='columns', inplace=True)
+C['Valores'] = A['Valores']
+C['Freq. Abs. (n=3)'] = l1,l2,l3,l4,l5,l6,l7,l8,l9,l10
+C['Freq. % (n=3)'] = round(C['Freq. Abs. (n=3)'] / sum(C['Freq. Abs. (n=3)'])*100,2)
+print(C)
+
+
+d4 = pd.DataFrame()
+n = 4
+d4['Amostras4'] = list(combinations(anos, n))
+l1=l2=l3=l4=l5=l6=l7=l8=l9=l10 = 0
+count = 0
+for i in range(0,len(d4['Amostras4'])):
+    for j in range(0,2):
+        if d4['Amostras4'][i][j] in i1:
+            l1 = l1 + 1
+        elif d4['Amostras4'][i][j] in i2:
+            l2 = l2 + 1
+        elif d4['Amostras4'][i][j] in i3:
+            l3 = l3 + 1
+        elif d4['Amostras4'][i][j] in i4:
+            l4 = l4 + 1
+        elif d4['Amostras4'][i][j] in i5:
+            l5 = l5 + 1
+        elif d4['Amostras4'][i][j] in i6:
+            l6 = l6+ 1
+        elif d4['Amostras4'][i][j] in i7:
+            l7 = l7 + 1
+        elif d4['Amostras4'][i][j] in i8:
+            l8 = l8 + 1
+        elif d4['Amostras4'][i][j] in i9:
+            l9 = l9 + 1
+        elif d4['Amostras4'][i][j] in i10:
+            l10 = l10 + 1
+
+print("\n")
+D = pd.DataFrame()
+D = pd.DataFrame(list([[l1],[l2],[l3],[l4],[l5],[l6],[l7],[l8],[l9],[l10]]))
+D.set_axis(['10 |-- 12','12 |-- 14','14 |-- 16','16 |-- 18','18 |-- 20','20 |-- 22','22 |-- 24',
+            '24 |-- 26','26 |-- 28','28 ou mais'], axis='rows', inplace=True)
+D.set_axis(['Valores'], axis='columns', inplace=True)
+D['Valores'] = A['Valores']
+D['Freq. Abs. (n=4)'] = l1,l2,l3,l4,l5,l6,l7,l8,l9,l10
+D['Freq. % (n=4)'] = round(D['Freq. Abs. (n=4)'] / sum(D['Freq. Abs. (n=4)'])*100,2)
+print(D)
